e4c227df8f7389ed5474c341a5ccf5622550d452
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2023-03-13 11:11:58 +0000

    Set alias to __dirname

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+console.log("dirname: ", __dirname);
+
 export default defineNuxtConfig({
+    alias: {
+        "@": resolve(__dirname),
+    },
     css: ["~/assets/main.scss"],
 });
