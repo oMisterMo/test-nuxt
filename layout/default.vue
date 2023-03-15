2b9477f344babe72e2a898ca6083e37f352b179f
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2023-03-15 23:06:33 +0000

    layouts typo

--- layout/default.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="default-layout">
-        <p>Default layout</p>
-        <slot />
-    </div>
-</template>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="default-layout bg-slate-200">
+        <!-- Navigation | Header | Footer -->
+        <!-- <p>Default layout</p> -->
+
+        <!-- Page content -->
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
