ff90b0952cfdde946a7bd7d81c419395798417f5
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2023-03-15 20:15:43 +0000

    Add tailwind support

--- app.vue
+++ app.vue
@@ -1,25 +1,5 @@
 <template>
-    <div>
-        <NuxtLayout>
-            <header>
-                <ul>
-                    <li>
-                        <NuxtLink to="/">Home</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to="/auth">Auth</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to="/search">Search</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to="/profile">Profile</NuxtLink>
-                    </li>
-                </ul>
-            </header>
-            <NuxtPage></NuxtPage>
-        </NuxtLayout>
-    </div>
+    <Mo />
 </template>
 
 <script setup>
