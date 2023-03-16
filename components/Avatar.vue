75e294188cc7e9d9979dacc72890f3f08ee46292
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2023-03-16 11:11:23 +0000

    Add simeple Avatar component to test props

---
+++ components/Avatar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>Source is {{ src }}</p>
+    </div>
+</template>
+
+<script setup>
+// This is only usable inside `<script setup>`, is compiled away in the
+// output and should **not** be actually called at runtime.
+const { src } = defineProps(["src"]);
+</script>
